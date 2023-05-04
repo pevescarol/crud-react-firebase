1d242f89138f5343b4ccef330871152b2bc1cc84
pevescarol <carol.peves@gmail.com>
2023-05-03 22:48:10 -0300

    se crea componentes y se agregan sus rutas en app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element='componente Show' />
+          <Route path='/create' element='Create' />
+          <Route path='/edit/:id' element='Edit' />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
