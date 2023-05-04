a328c201e7d416c4cb0a357e5e3d01b364faff0f
pevescarol <carol.peves@gmail.com>
2023-05-03 23:18:48 -0300

    se agrega funcion para eliminar doc en Show

--- src/components/Show.jsx
+++ src/components/Show.jsx
@@ -24,4 +24,44 @@ export const Show = () => {
       data.docs.map( doc => ({...doc.data(), id:doc.id}))
     )
   }
+
+  //4. funcion para eliminar un doc
+  const deleteHeroe = async (id)=> {
+    const heroeDoc = doc(db, 'heroes', id)
+    await deleteDoc(heroeDoc)
+    // getHeroes()
+  }
+
+  //5. funcion para la confirmacion de sweet alert
+  const confirmDelete = (id)=> {
+    Swal.fire({
+      title: '¿Estas segurx?',
+      text: "No podrás revertir esta accion!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, deseo borrarlo!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteHeroe(id) // llamamos a la funcion eliminar
+        Swal.fire(
+          'Borrado!',
+          'Tu heroe ha sido eliminado',
+          'success'
+        )
+      }
+    })
+  }
+
+  //6. useEffect
+  useEffect( () => {
+    getHeroes()
+  }, [])
+
+  //7. devolver la vista de nuestra componente
+  return(
+    <>
+    </>
+  )
 }
